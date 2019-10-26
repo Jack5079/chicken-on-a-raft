845666a9938c9b653d9f4575bcc9d393c9ad905c
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-26 18:46:53 -0400

    switch to sound-play

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('console-png').attachTo(console)
 const { join } = require('path')
 console.png(require('fs').readFileSync(join(__dirname, '/chicken.png')))
 var time = 0
-var player = require('play-sound')({})
+const { play } = require('sound-play')
 
 
 var rtf1 = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
@@ -14,6 +14,4 @@ function timer () {
   console.log(`You have been on a raft with a chicken for ${out[0].value}${out[1].value}.`)
 }
 setInterval(timer, 1000)
-player.play(join(__dirname, '/sound.mp3'), function (err) {
-  if (err) throw err
-})
+play(join(__dirname, '/sound.mp3'))
