ddba41e9a67a5ecbe29384d4ff7a967608dc9209
Jack <29169102+Jack5079@users.noreply.github.com>
2019-10-25 21:34:35 -0400

    idk

--- index.js
+++ index.js
@@ -5,65 +5,13 @@ console.png(require('fs').readFileSync(join(__dirname, '/chicken.png')))
 var time = 0
 var player = require('play-sound')({})
 
-function formatTime () {
-  var res = ''
-  var hours = 0
-  var mins = 0
-  var secs = time
 
-  while (secs >= 60) {
-    secs -= 60
-    mins += 1
-  }
-
-  while (mins >= 60) {
-    mins -= 60
-    hours += 1
-  }
-
-  if (secs === 0) {
-    secs = ''
-  } else if (secs === 1) {
-    secs += ' second'
-  } else if (secs > 1) {
-    secs += ' seconds'
-  }
-
-  if (mins === 0) {
-    mins = ''
-  } else if (mins === 1) {
-    mins += ' minute'
-  } else if (mins > 1) {
-    mins += ' minutes'
-  }
-
-  if (hours === 0) {
-    hours = ''
-  } else if (hours === 1) {
-    hours += ' hour'
-  } else if (hours > 1) {
-    hours += ' hours'
-  }
-
-  if (hours !== '') {
-    res += hours + ' '
-  }
-
-  if (mins !== '') {
-    res += mins + ' '
-  }
-
-  if (secs !== '') {
-    res += secs
-  }
-
-  return res
-}
-
+var rtf1 = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
 function timer () {
   time++
-  var out = formatTime()
-  console.log(`You have been on a raft with a chicken for ${out}.`)
+  var out = rtf1.formatToParts(time, 'seconds')
+  out.shift()
+  console.log(`You have been on a raft with a chicken for ${out[0].value}${out[1].value}.`)
 }
 setInterval(timer, 1000)
 player.play(join(__dirname, '/sound.mp3'), function (err) {
